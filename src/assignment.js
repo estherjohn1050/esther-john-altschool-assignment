37b1a18cc7ae1865b74f6603664f9373af6e0f24
EstherJohn Esther <>
2022-07-13 00:47:11 +0000

    check assignment 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+const arrayOfNumbers = [1,2,3,4]
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for (let i = 0; i <arrayOfNumbers.length; i++) {
+   sum+= arrayOfNumbers[i] 
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+console.log (sumOfNumbers(arrayOfNumbers))
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let count = 0;
+  for (let i = 0; i <arrayOfNumbers.length; i++) {
+    if(arrayOfNumbers[i] % 2 == 0){
+      count++
+    }
+  }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+console.log (countEvenNumbers(arrayOfNumbers))
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
